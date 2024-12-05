ed9e224fcc75f6b6d9e164fbb68056cf1e44ab42
여준서 <duwnstj12@naver.com>
2024-12-05 17:57:09 +0900

    git init

---
+++ src/main/java/com/sparta/intern_homework/InternHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.intern_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternHomeworkApplication.class, args);
+    }
+
+}
